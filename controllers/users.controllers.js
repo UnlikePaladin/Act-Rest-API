dda2d60a1eb9c42ccbfdc3379913decb8eec0afa
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-03-10 15:50:11 -0600

    user type things

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -5,3 +5,13 @@ export const getUsers = async (req, res) => {
     const data = await sql.query("select * from users");
     return res.json(data);
 };
+
+export const getUser = async (req, res) => {
+    const sql = connectDB();
+    const query = {
+        text : "select * from users where user_id = $1",
+        values : [req.params.id]
+    };
+    const data = await sql.query(query);
+    return res.json(data);
+};
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users.controllers.js";
+import { getUsers, getUser } from "../controllers/users.controllers.js";
 
 const router = Router();
 
 router.get("/users", getUsers);
+router.get("/user/:id", getUser);
 
 export default router;
