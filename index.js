1228f103e0750bf2f3955570a3fcd5aa6b15bde9
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-03-30 20:07:34 -0600

    add cors

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import "dotenv/config";
 import express from "express";
 import indexRoutes from "./routes/index.routes.js";
 import userRoutes from "./routes/users.routes.js";
+import cors from "cors";
 import loginRoutes from "./routes/login.routes.js";
+import morgan from "morgan";
 
 const app = express();
 const port = 4000;
 
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(indexRoutes);
 app.use(userRoutes);
